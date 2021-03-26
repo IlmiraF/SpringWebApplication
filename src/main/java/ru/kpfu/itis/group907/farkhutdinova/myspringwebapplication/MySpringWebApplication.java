10f7f735203b55fcf06115fd3b92870ae11c8fde
Ilmira Farkhutdinova <farkhutdinova.ilmira@bk.ru>
2021-03-26 18:03:41 +0300

    Commit 1

---
+++ src/main/java/ru/kpfu/itis/group907/farkhutdinova/myspringwebapplication/MySpringWebApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.group907.farkhutdinova.myspringwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kpfu/itis/group907/farkhutdinova/myspringwebapplication/MySpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.group907.farkhutdinova.myspringwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
